9002816dddbe3dc94d38128620ea1f72477801b2
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-07-27 22:40:10 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+from art import logo
+
+def add(n1, n2):
+  return n1 + n2
+
+def subtract(n1, n2):
+  return n1 - n2
+
+def multiply(n1, n2):
+  return n1 * n2
+
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
+
+def calculator():
+  print(logo)
+
+  num1 = float(input("Введите первое число: "))
+  for symbol in operations:
+    print(symbol)
+  should_continue = True
+ 
+  while should_continue:
+    operation_symbol = input("Выберите операцию: ")
+    num2 = float(input("Введите следующее число: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if input(f"Введите 'y' чтобы продолжить операцию с {answer}, или 'n' чтобы начать новую: ") == 'y':
+      num1 = answer
+    else:
+      should_continue = False
+      calculator()
+
+calculator()
